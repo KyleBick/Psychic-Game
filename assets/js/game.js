232e6bce503065b4b3e398a251222384e9aa4939
Kyle Bick <kyle.g.bick@gmail.com>
2018-08-18 09:42:50 -0400

    Finished

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,36 +1,40 @@
+//Create var's for wins, losses, attempts, used letters empty array, random letter for comp to pick from and letters in alphabet to pick from
 var won = 0;
-var lost = 0;
+var losses = 0;
 var attempts = 10;
 var usedArray = [];
 var randLetter = randLetter;
 var letters = "abcdefghijklmnopqrstuvwxyz";
 
+//computer needs to pick random letter
 randLetter = letters[Math.floor(Math.random() * letters.length)];
-console.log(randLetter);
+
 
 function compGuess() {
     randLetter = letters[Math.floor(Math.random() * letters.length)];
     console.log(randLetter);
 }
 
+//players input 
 document.onkeyup = function (event) {
 var playerGuess = event.key;
 
-
+//give users 10 guesses and add win to total wins
 if (playerGuess === randLetter) {
     won++;
     attempts = 10;
     usedArray = [];
 }
 
+//deductattempts from user for guessing wrong letter
 compGuess();
 
 if (playerGuess !== randLetter){
     attempts--;
 }
-
+// if attempts reach 0 out of 10 player loses 
 if (attempts == 0) {
-    lost++;
+    losses++;
     usedArray = [];
     attempts = 10;
 }
@@ -38,6 +42,7 @@ if (attempts == 0) {
 if (usedArray.indexOf(playerGuess) >= 0) {
 }
 
+// push js to html by attatching it to an ID for displaying wins losses attempts and guessed letters
 else {
     usedArray.push(playerGuess);
     document.getElementById('playerGuess').innerHTML = usedArray;
@@ -45,7 +50,7 @@ else {
 }
 
 document.getElementById('won').innerHTML = won;
-document.getElementById('lost').innerHTML = lost;
+document.getElementById('losses').innerHTML = losses;
 document.getElementById('attempts').innerHTML = attempts;
 
 }
